5d74423edf543333518a5b0203821c6cf2b845d2
Julia Loftin <julia.loftin7@gmail.com>
2023-11-29 17:57:48 -0600

    completes task three

--- queries.sql
+++ queries.sql
@@ -1,7 +1,16 @@
 --Part 1
-
+/*
+id - INT
+employer - VARCHAR
+name - VARCHAR
+skills - VARCHAR
+*/
 --Part 2
-
+/*
+SELECT name FROM employer WHERE location = "St. Louis City";
+*/
 --Part 3
-
+/*
+DROP TABLE job;
+*/
 --Part 4
--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,5 +1,47 @@
 package org.launchcode.techjobs.persistent.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToMany;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
 public class Skill extends AbstractEntity {
 
+    @ManyToMany(mappedBy = "skills")
+    private Job job;
+
+    @NotBlank
+    @Size(max = 250, message = "Description may not be longer than 250 characters")
+    private String description;
+
+    public Skill(String description) {
+        this.description = description;
+    }
+
+    public Skill() {}
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Job getJob() {
+        return job;
+    }
+
+    public void setJob(Job job) {
+        this.job = job;
+    }
+
+    @Override
+    public String toString() {
+        return "Skill{" +
+                "job=" + job +
+                ", description='" + description + '\'' +
+                '}';
+    }
 }
